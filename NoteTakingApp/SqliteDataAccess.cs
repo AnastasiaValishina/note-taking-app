a486f245b98abc43a79896c6e5c7f32f3b299190
AnastasiaValishina <azhidenova@gmail.com>
2024-01-04 14:05:09 +0100

    sqlite + dapper installed

---
+++ NoteTakingApp/SqliteDataAccess.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteTakingApp
+{
+    public class SqliteDataAccess
+    {
+        public static List<Note> LoadNotes()
+        {
+            using(IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<Note>("select * from Note", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+        public static void SaveNote(Note note) 
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+
+            }
+        }
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
